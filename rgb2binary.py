0f891ea731691067e9ea075c55c58180517bdc6a
mksarker <m.kamal.sarker@gmail.com>
2019-04-02 02:00:58 +0200

    updated

--- rgb2binary.py
+++ rgb2binary.py
@@ -1,1 +1,17 @@
+import os
 import cv2
+import skimage.color as color
+import numpy as np
+
+data_dir='path to the data directory'
+dest_dir='path to the destination directory'
+
+files= os.listdir(data_dir)
+
+for f in files:
+    im=cv2.imread(data_dir+f)
+    im= color.rgb2gray(im)
+    cv2.imwrite(dest_dir+f, im)
+    gt=cv2.imread(dest_dir+f)
+    unq=np.unique(gt)
+    print('after', unq)# for checking the binary values in every image
