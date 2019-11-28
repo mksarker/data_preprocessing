361495a1d084232d5878fa226bb558938f71bf90
Md. Mostafa Kamal Sarker <m.kamal.sarker@gmail.com>
2019-11-28 15:21:37 +0100

    Update image_view_pytorch.py

--- image_view_pytorch.py
+++ image_view_pytorch.py
@@ -1,5 +1,5 @@
 ## after apply any leyer of the forward function in pytorch 
-img=x1[:,:3,:,]
+img=x1[:,:3,:,].cpu().data.numpy()
 img=np.transpose(img,(1,2,0))
 cv2.imshow('out',img)
 cv2.waitKey(0)
