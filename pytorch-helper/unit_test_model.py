d02d9120e89a78d505d4006b2d0219ee72021a68
Md. Mostafa Kamal Sarker <m.kamal.sarker@gmail.com>
2020-05-16 22:11:31 +0200

    Update unit_test_model.py

--- pytorch-helper/unit_test_model.py
+++ pytorch-helper/unit_test_model.py
@@ -4,6 +4,10 @@ import torch.nn as nn
 from emanet import EMANet
 from ptflops import get_model_complexity_info
 from torchvision import models
+from efficientnet_pytorch import EfficientNet
+
+from pytorch_memlab import MemReporter
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -11,22 +15,35 @@ def count_parameters(model):
 class TestResNetWrapper(unittest.TestCase):
 
     def test_resnet_wrapper(self):
-        net = models.resnet50(num_classes=1000)
+        net = EfficientNet.from_pretrained("efficientnet-b3")
 
         # print(net)
         # params = list(net.parameters()) 
         # count=count_parameters(net)
         # print ("Params:",count/1000000)
 
+        ## params and MACs
         macs, params = get_model_complexity_info(net, (3, 244, 244), as_strings=True,
                                         print_per_layer_stat=True, verbose=True)
         print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
         print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 
+        ### Input for check model
         x = torch.rand(2, 3, 224, 224)
         x = torch.autograd.Variable(x)
+       
+        ## model size
+        mem_params = sum([param.nelement()*param.element_size() for param in net.parameters()])
+        mem_bufs = sum([buf.nelement()*buf.element_size() for buf in net.buffers()])
+        mem = mem_params + mem_bufs # in bytes
+        print('Memory Size:', mem/1000000)
+        ## Memory 
+        # reporter = MemReporter(net)
+        # reporter.report()
+
+        ### model implementation
         x = net(x)
-        # print(x.size())
+
         self.assertTrue(x is not None)
 
 if __name__ == '__main__':
