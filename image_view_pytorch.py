85e82a85f5a00a7d2dec21de1194c73eca004633
Md. Mostafa Kamal Sarker <m.kamal.sarker@gmail.com>
2020-11-03 11:11:40 +0000

    Update image_view_pytorch.py

--- image_view_pytorch.py
+++ image_view_pytorch.py
@@ -1,5 +1,9 @@
 ## after apply any leyer of the forward function in pytorch 
-img=x1[:,:3,:,].cpu().data.numpy()
+iimg=input[:,:3,:,].cpu().data.numpy()
+# print(img.shape)
+img = np.squeeze(img)
+# print(img.shape)
 img=np.transpose(img,(1,2,0))
+img = cv2.resize(img,(256,256))
 cv2.imshow('out',img)
 cv2.waitKey(0)
